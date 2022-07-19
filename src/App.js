ca41ac255f52dd8b4e08de27eb36d5155a290e25
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-07-19 10:55:44 +0200

    esercizio jsx 02 in branch jsx-02

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-    const hello = <h1>Hello, World!</h1>
-    return <div>{hello}</div>
-  }
+  render () {
+    const name = 'Jimmy'
+    const hello = <h1>Hello, {name}</h1>
+      return <div>{hello}</div>
+}
 }
 
 export default App;
